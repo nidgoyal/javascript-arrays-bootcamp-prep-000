4a44812cb470fa40f3df2f121d41ccb5e5b8564f
Nidhi <nidgoyal1@gmail.com>
2017-12-31 02:58:51 +0000

    automatically backed up by learn

---
+++ arrays.js
@@ -0,0 +1,24 @@
+var ingredient1 = "bread"
+var ingredient2 = "mild cheese"
+var ingredient3 = "sharp cheese"
+var ingredient4 = "butter"
+var ingredient5 = "tomato"
+var ingredient6 = "garlic"
+
+var chocolateBars= [
+  'snickers',
+  'hundred grand',
+  'kitkat',
+  'skittles'
+];
+chocolateBars();
+
+var array= [];
+function addElementToBeginningOfArray(){
+  array.unshift("",'foo')
+  return array;
+}
+addElementToBeginningOfArray();
+
+
+
