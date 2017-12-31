687bbb765629aeda06b4de54f92cfdb90974f32d
Nidhi <nidgoyal1@gmail.com>
2017-12-31 03:55:35 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -49,3 +49,10 @@ function removeElementFromBeginningOfArray(array){
   return array.slice(1);
 }
 
+function destructivelyRemoveElementFromEndOfArray(array){
+  array.pop();
+  return array;
+}
+
+
+function
